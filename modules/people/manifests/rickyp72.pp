58a77f9f13f98e9bb2465d1deaad0dd5b8f7d762
ricky <rickyp72@googmail.com>
2014-01-13 10:42:48 +0000

    set dock dim hidden

--- modules/people/manifests/rickyp72.pp
+++ modules/people/manifests/rickyp72.pp
@@ -2,10 +2,11 @@ class people::rickyp72 {
   notify { 'Hello Ricky': }
   include vagrant
 
-# ... or set your own
-class { 'osx::dock::icon_size':
-  size => 20
-}
+	# ... or set your own
+	class { 'osx::dock::icon_size':
+	  size => 20
+	}
+	include osx::dock::dim_hidden_apps
 
   case $::hostname {
     'users-Mac': {
