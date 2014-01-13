80e5fc0d63366ca3a1a0b724b710a8020228ed55
ricky <rickyp72@googmail.com>
2014-01-13 16:31:21 +0000

    added my mbp computer case

--- modules/people/manifests/rickyp72.pp
+++ modules/people/manifests/rickyp72.pp
@@ -2,20 +2,25 @@ class people::rickyp72 {
   notify { 'Hello Ricky': }
   include vagrant
 
-	# ... or set your own
-	class { 'osx::dock::icon_size':
-	  size => 20
-	}
+
 	include osx::dock::dim_hidden_apps
 
   case $::hostname {
     'users-Mac': {
-	notify { "This is users-Mac": }
+		class { 'osx::dock::icon_size':
+		  size => 20
+		}
     }
-
+    'Rickys-MacBook-Pro-2': {
+      notify { "This is my Macbook Pro Boxen": }
+    }
     default: {
-	notify { "This is the default!": }
+	  notify { "This is the default!": }
    }
+   	# ... or set your own
+	class { 'osx::dock::icon_size':
+	  size => 20
+	}
  }
 
    package {
