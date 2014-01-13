89a47a79666532f661d3dbac4a0b5beb7d3a6ee0
ricky <rickyp72@googmail.com>
2014-01-13 16:35:09 +0000

    tided up case statements

--- modules/people/manifests/rickyp72.pp
+++ modules/people/manifests/rickyp72.pp
@@ -1,15 +1,20 @@
 class people::rickyp72 {
   notify { 'Hello Ricky': }
-  include vagrant
-
-
-	include osx::dock::dim_hidden_apps
 
   case $::hostname {
+  	  include vagrant
+  	  include osx::dock::dim_hidden_apps
     'users-Mac': {
 		class { 'osx::dock::icon_size':
 		  size => 20
 		}
+		   package {
+	   	[
+	   	  'nmap',
+	   	  'emacs',
+	   	  ]:
+	   	  ensure => 'present',
+	   }
     }
     'Rickys-MacBook-Pro-2': {
       notify { "This is my Macbook Pro Boxen": }
