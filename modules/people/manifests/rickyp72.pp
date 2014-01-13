9acfb1129b8c14e2506bed2afac60b81a447290d
ricky <rickyp72@googmail.com>
2014-01-13 10:39:34 +0000

    set dock size

--- modules/people/manifests/rickyp72.pp
+++ modules/people/manifests/rickyp72.pp
@@ -2,9 +2,6 @@ class people::rickyp72 {
   notify { 'Hello Ricky': }
   include vagrant
 
-  # Set the default value (36)
-include osx::dock::icon_size
-
 # ... or set your own
 class { 'osx::dock::icon_size':
   size => 20
