5b9b4581cb03d1750c292641b67d45349d776c9c
ricky <rickyp72@googmail.com>
2014-01-14 14:57:54 +0000

    removed case

--- modules/people/manifests/rickyp72.pp
+++ modules/people/manifests/rickyp72.pp
@@ -1,27 +1,5 @@
 class people::rickyp72 {
   notify { 'Hello Ricky': }
 
-  case $::hostname {
-  #   'users-Mac': {
-  #   	include vagrant
-  # 	    include osx::dock::dim_hidden_apps
-		# class { 'osx::dock::icon_size':
-		#   size => 20
-		# }
-		  #  package {
-	   # 	[
-	   # 	  'nmap',
-	   # 	  'emacs',
-	   # 	  ]:
-	   # 	  ensure => 'present',
-	   # }
-    }
 
-    'Rickys-MacBook-Pro-2': {
-      notify { "This is my Macbook Pro Boxen": }
-    }
-    default: {
-	  notify { "This is the default!": }
-   }
- }
 }
